93fbd63874c2aa9cd090d3f3077d2a6123eb88d0
David Nguyen <david@wthapps.com>
2017-10-29 15:47:00 +0700

    update build stone

--- recursing/build-stone.py
+++ recursing/build-stone.py
@@ -1,15 +1,22 @@
 def buildStone(a, n):
-    if n == 0: return 0;
-    if n == 1: return 1;
-    count = 1;
-    b = []
-    first = a[0]
-    for i in range(1, n):
-        if a[i] < first:
-            b = trimZero(b)
-            count += buildStone(b, len(b)) + 1
+    if n == 0: return 0
+    if n == 1: return 1
+    count = 1
+    tmp = a[0]
+    index = 0
+    for i in range(n):
+        if a[i] < tmp:
+            b = trimZero(a[index:i])
+            count += buildStone(b, len(b))
+
+            b = trimZero(a[i:])
+            count += buildStone(b, len(b))
+            return count
         else:
-            b.append(a[i] - first)
+            a[i] = a[i] - tmp
+            if i == n -1:
+                b = trimZero(a[index:])
+                count += buildStone(b, len(b))
     return count
 
 def trimZero(a):
@@ -19,5 +26,7 @@ def trimZero(a):
         a = a[0:len(a)-1]
     return a
 
-arr = [8,8,5,7,9,8,7,4,8]
+arr = [8,8,5,7,9,8,7,4,8,8]
+# arr = [8,8,5,7,9,8,7,4,8,8,5]
+# arr = [8,5]
 print(buildStone(arr, len(arr)))
