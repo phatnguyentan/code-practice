7ca4a10b9220c1b94b16e7308b2c083c57fa3dd1
David Nguyen <david@wthapps.com>
2017-11-24 09:34:33 +0700

    new

--- linked-list/delete-reverse-loop-merge.py
+++ linked-list/delete-reverse-loop-merge.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self, data):
+    def __init__(self, data = None):
         self.data = data
         self.next = None
 
@@ -47,12 +47,47 @@ class LinkedList:
             node = node.next
         return False
 
+    def merge(self, list):
+        node1 = self.head
+        node2 = list.head
+        l = LinkedList()
+        l.head = Node()
+
+        while node1 and node2:
+            if node1.data > node2.data:
+                n = node2.next
+                self.move(node2, l.head)
+                node2 = n
+            else:
+                n = node1.next
+                self.move(node1, l.head)
+                node1 = n
+
+        return l
+
+    def move(self, src, new):
+        # new = src
+        # src = new.next
+        # new.next = des
+        # des = new
+        src.next = new
+        new.next = src
+
 l = LinkedList()
 l.head = Node(1)
-l.head.next = Node(2)
-l.head.next.next = Node(3)
-l.head.next.next.next = Node(4)
+l.head.next = Node(3)
+l.head.next.next = Node(4)
+l.head.next.next.next = Node(6)
+l2 = LinkedList()
+l2.head = Node(2)
+l2.head.next = Node(4)
+l2.head.next.next = Node(5)
+l2.head.next.next.next = Node(7)
+l3 = l.merge(l2)
+print(l3.head.data)
+print(l3.head.next.data)
+print(l3.head.next.next.data)
 # l.head.next.next.next.next = l.head.next
 # l.reverse()
-print(l.isLoop())
+# print(l.isLoop())
 # l.list()
