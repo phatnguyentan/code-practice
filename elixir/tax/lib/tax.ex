b02929ae6265ad9c7a73b49e30a6c8835fa34a23
David Nguyen <david@wthapps.com>
2018-02-09 18:05:47 +0700

    update

--- elixir/tax/lib/tax.ex
+++ elixir/tax/lib/tax.ex
@@ -18,6 +18,11 @@ defmodule Tax do
 
   def start(_a, _b) do
     IO.puts("aaaaaaaaaaaa")
+    NimbleCSV.define(MyParser, separator: "\t", escape: "\"")
+
+    # Parse the data
+    IO.inspect MyParser.parse_string "name\tage\njohn\t27"
+    #=> [["john","27"]]
     # List all child processes to be supervised
     children = [
       # Starts a worker by calling: A.Worker.start_link(arg)
