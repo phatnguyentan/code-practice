a819786d535f837e9ffd34f0dc02ed0679ac1f21
Phat Nguyen <phatnguyen@lifung.com>
2021-04-10 14:54:44 +0700

    update

--- frontend/rxjs/combination/combineAll/combineAll.ts
+++ frontend/rxjs/combination/combineAll/combineAll.ts
@@ -2,7 +2,7 @@ import { take, map, combineAll } from 'rxjs/operators';
 import { interval } from 'rxjs';
 
 // emit every 1s, take 2
-const source$ = interval(1000).pipe(take(2));
+const source$ = interval(3000).pipe(take(2), map(val => "source" + val));
 // map each emitted value from source to interval observable that takes 5 values
 const example$ = source$.pipe(
   map(val =>
---
+++ frontend/rxjs/transformation/mergeMap.ts
@@ -0,0 +1,7 @@
+import { interval, of } from "rxjs";
+import { map, mergeMap, take } from "rxjs/operators";
+
+const source = of("a", "b", "c");
+source.pipe(
+    mergeMap(val => interval(1000).pipe(map(i => "source " + val + " i " + i),take(4)))
+).subscribe(console.log);
