99fd5462c8545d878680b4d18a6bbba9d7414e82
Phat Nguyen <phatnguyen@lifung.com>
2021-04-11 16:02:00 +0700

    update

---
+++ frontend/rxjs/error-handling/catch/catch.ts
@@ -0,0 +1,9 @@
+// RxJS v6+
+import { throwError, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+//emit error
+const source = throwError('This is an error!');
+//gracefully handle error, returning observable with error message
+const example = source.pipe(catchError(val => of(`I caught: ${val}`)));
+//output: 'I caught: This is an error'
+const subscribe = example.subscribe(val => console.log(val));
